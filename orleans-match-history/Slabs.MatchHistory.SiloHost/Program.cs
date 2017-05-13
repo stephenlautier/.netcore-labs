5c0e90bc6359ecfa28a53a385334924d40a0c9ab
Stephen Lautier <stephen.lautier@outlook.com>
2017-05-13 02:09:52 +0200

    initial orleans-match-history app

---
+++ orleans-match-history/Slabs.MatchHistory.SiloHost/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Slabs.MatchHistory.SiloHost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddCommandLine(args)
+				.AddJsonFile("app-config.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"app-config.{environment}.json", optional: true)
+				.AddEnvironmentVariables();
+
+			var config = builder.Build();
+
+			var loggerFactory = new LoggerFactory();
+
+			var serilog = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.ColoredConsole()
+				.CreateLogger();
+
+			var logger = loggerFactory.AddSerilog(serilog)
+					.AddDebug(LogLevel.Trace)
+					.CreateLogger<Program>();
+
+			logger.LogInformation("Starting silo... Env={environment} Machine={machine}", environment, Environment.MachineName);
+
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
