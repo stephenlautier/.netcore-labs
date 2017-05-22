2c957ffb148e9f0e666a141bbfca56c05772c324
Stephen Lautier <stephen.lautier@outlook.com>
2017-05-23 01:09:42 +0200

    add healthmiddleware

---
+++ simple-docker-api/Slabs.SimpleDockerApi/Infrastructure/HealthMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Slabs.SimpleDockerApi.Infrastructure
+{
+	// ReSharper disable once ClassNeverInstantiated.Global
+	public class HealthMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public HealthMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext httpContext)
+		{
+			httpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+			await httpContext.Response.WriteAsync("Health status ok");
+		}
+	}
+
+	public static class HealthMiddlewareExtensions
+	{
+		// ReSharper disable once UnusedMethodReturnValue.Global
+		public static IApplicationBuilder UseHealthMiddleware(this IApplicationBuilder builder, string path = "/health")
+		{
+			return builder.Map(path, subApp => subApp.UseMiddleware<HealthMiddleware>());
+		}
+	}
+}
