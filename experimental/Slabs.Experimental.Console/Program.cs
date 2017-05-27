42d7daf7d9bb9aaaaa9c4075c947333c4a33347f
Stephen Lautier <stephen.lautier@outlook.com>
2017-05-28 01:24:37 +0200

    add experimental project
    + add test suite initial experimental

---
+++ experimental/Slabs.Experimental.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Slabs.Experimental.ConsoleClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RunTestSuite();
+			Console.WriteLine("Press any key to stop...");
+			Console.ReadKey();
+		}
+
+		private static async void RunTestSuite()
+		{
+			Console.WriteLine("Init TestSuite...");
+			var gamingTestSuite = new TestSuiteBuilder("gaming")
+				.Add("get-teams")
+				.Add("add-teams")
+				.Add("get-team-detail")
+				.Build();
+
+			await gamingTestSuite.Run();
+
+		}
+
+	}
+
+	
+}
---
+++ experimental/Slabs.Experimental.Console/TestSuite.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slabs.Experimental.ConsoleClient
+{
+	internal class TestSuiteBuilder
+	{
+		private readonly string _name;
+
+		private List<ITest> Tests { get; }
+
+
+		public TestSuiteBuilder(string name)
+		{
+			Tests = new List<ITest>();
+			_name = name;
+		}
+
+		public TestSuite Build()
+		{
+			return new TestSuite(_name, Tests);
+		}
+
+		public TestSuiteBuilder Add(string key)
+		{
+			Tests.Add(new TestEntity { Key = key });
+			return this;
+		}
+	}
+
+	internal class TestSuite
+	{
+		public string Name { get; }
+		private readonly List<ITest> _tests;
+
+		public TestSuite(string name, List<ITest> tests)
+		{
+			Name = name;
+			_tests = tests;
+		}
+
+		public Task Run()
+		{
+			Console.WriteLine($"[TestSuite] Running tests for '{Name}'");
+			foreach (var test in _tests)
+			{
+				Console.WriteLine($"[TestSuite] Running {test.Key}");
+			}
+			return Task.CompletedTask;
+		}
+	}
+
+	internal interface ITest
+	{
+		string Key { get; set; }
+		//Task Execute();
+	}
+
+	internal class TestEntity : ITest
+	{
+		public string Key { get; set; }
+	}
+
+}
