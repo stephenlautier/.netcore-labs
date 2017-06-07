3e76aa2acd8a09f1e648ae329918f3d7d94bc1b7
Stephen Lautier <stephen.lautier@outlook.com>
2017-06-07 16:20:06 +0200

    fluent http: extract middleware runner to seperate file

--- experimental/Slabs.Experimental.Console/FluentHttp/FluentHttpClient.cs
+++ experimental/Slabs.Experimental.Console/FluentHttp/FluentHttpClient.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -96,50 +95,7 @@ namespace Slabs.Experimental.ConsoleClient.FluentHttp
 			return httpClient;
 		}
 	}
-
-	public interface IFluentHttpMiddlewareRunner
-	{
-		Task<IFluentHttpResponse> Run<T>(IList<Type> middleware, FluentHttpRequest request, FluentHttpRequestDelegate send);
-	}
-
-	public class FluentHttpMiddlewareRunner : IFluentHttpMiddlewareRunner
-	{
-		private readonly IServiceProvider _serviceProvider;
-
-		public FluentHttpMiddlewareRunner(IServiceProvider serviceProvider)
-		{
-			_serviceProvider = serviceProvider;
-		}
-
-		public async Task<IFluentHttpResponse> Run<T>(IList<Type> middleware, FluentHttpRequest request, FluentHttpRequestDelegate send)
-		{
-			if (middleware.Count == 0)
-				return await send(request);
-
-			IFluentHttpResponse httpResult = null;
-			IFluentHttpMiddleware previousMiddleware = null;
-
-			for (int i = middleware.Count; i-- > 0;)
-			{
-				var type = middleware[i];
-
-				var isLast = middleware.Count - 1 == i;
-				var isFirst = i == 0;
-				var next = isLast
-					? send
-					: previousMiddleware.Invoke;
-				var instance = (IFluentHttpMiddleware)ActivatorUtilities.CreateInstance(_serviceProvider, type, next);
-
-				if (isFirst)
-					httpResult = await instance.Invoke(request);
-				else
-					previousMiddleware = instance;
-			}
-			return httpResult;
-		}
-
-	}
-
+	
 	public class FluentHttpClientOptions
 	{
 		public string BaseUrl { get; set; }
