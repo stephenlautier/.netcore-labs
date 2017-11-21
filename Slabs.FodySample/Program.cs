1d9b43c84b24a7425d07b875fe71082b3b8a00d0
Stephen Lautier <stephen.lautier@outlook.com>
2017-11-22 00:28:57 +0100

    feat(fody): rename + installed

---
+++ Slabs.FodySample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NullGuard;
+
+namespace Slabs.FoldySample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			SomeMethod(null);
+		}
+
+		public static void SomeMethod(string arg)
+		{
+			// throws ArgumentNullException if arg is null.
+			Console.WriteLine($"Argument {arg.ToString()}");
+		}
+
+		public void AnotherMethod([AllowNull] string arg)
+		{
+			// arg may be null here
+		}
+
+	}
+}
