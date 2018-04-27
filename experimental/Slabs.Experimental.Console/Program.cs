f7d528d9e6de0cf00920d2d222c90202453fcd58
Stephen Lautier <stephen.lautier@outlook.com>
2018-04-27 21:05:21 +0200

    experimental: add config + fix logger to use serilog consistently

--- experimental/Slabs.Experimental.Console/Program.cs
+++ experimental/Slabs.Experimental.Console/Program.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Slabs.Experimental.ConsoleClient.Pipe;
 using Slabs.Experimental.ConsoleClient.Testify;
-using System;
 
 namespace Slabs.Experimental.ConsoleClient
 {
@@ -11,12 +12,21 @@ namespace Slabs.Experimental.ConsoleClient
 	{
 		static void Main(string[] args)
 		{
+			var configBuilder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddCommandLine(args)
+				.AddJsonFile("config.json");
+
+			var config = configBuilder.Build();
+
 			var loggerConfig = new LoggerConfiguration()
 				.WriteTo.Console()
-				.CreateLogger();
+				.ReadFrom.Configuration(config)
+				.CreateLogger()
+				;
 
 			var serviceProvider = new ServiceCollection()
-				.AddLogging()
+				.AddLogging(x => x.AddSerilog(loggerConfig))
 				.AddScoped<TestSuiteStartup>()
 				.AddScoped<PipeTestStartup>()
 				.AddSingleton<TestSuiteBuilderFactory>()
@@ -26,9 +36,6 @@ namespace Slabs.Experimental.ConsoleClient
 				.BuildServiceProvider();
 
 			var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-			loggerFactory.AddSerilog(loggerConfig)
-				.AddDebug();
-
 			var logger = loggerFactory.CreateLogger<Program>();
 
 			//var startup = serviceProvider.GetService<TestSuiteStartup>();
